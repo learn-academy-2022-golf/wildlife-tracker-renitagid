05eb0ec958f037149242c160bd094d2773a78981
renitagid <renita.y.ip@gmail.com>
2022-12-13 14:05:08 -0800

    story 4 complete

--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,3 +1,7 @@
 class Animal < ApplicationRecord
     has_many :sightings
+    accepts_nested_attributes_for :sightings
+    validates :name, :binomial, presence: true
+    validates :name, comparison: { other_than: :binomial }
+    validates :name, :binomial, uniqueness: true
 end
