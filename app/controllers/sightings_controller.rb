85d822926eab8e1c24054243fe077346716c386d
renitagid <renita.y.ip@gmail.com>
2022-12-13 13:04:28 -0800

    indentations

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -8,8 +8,6 @@ class SightingsController < ApplicationController
         def show
             sighting = Sighting.find(params[:id])
             render json: sighting
-            
-
         end
     
         def create
